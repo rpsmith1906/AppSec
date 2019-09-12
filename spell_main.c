80e434f993639835ece534caf2c3d1c370d9b488
Robert Smith <robert.p.smith@nyu.edu>
2019-09-11 23:01:06 -0400

    Completed main function

---
+++ spell_main.c
@@ -0,0 +1,42 @@
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#include "dictionary.h"
+
+int main (int argc, char *argv[] )
+{
+     char *misspelled[1000] ;
+     char verb[5] = "were" ;
+     FILE *fp ;
+     int i,mswords ;
+
+     hashmap_t hashtable[HASH_SIZE] ;
+
+
+     if ( argc < 3 ) 
+     {
+          printf("Usage %s <input file> < word list >...\n", argv[0] ) ;
+          return (1) ;
+     }
+
+     if (( fp = fopen(argv[1], "r")) == NULL )
+     {
+          printf ( "Error! could not open file \n" ) ;
+     }
+
+     load_dictionary( argv[1], hashtable ) ;
+     mswords=check_words(fp, hashtable, misspelled) ;
+
+     if ( mswords > 0 ) 
+     { 
+          if ( mswords == 1 ) { strcpy ( verb, "was" ) ; }
+          printf("There %s %i misspelled words found.\n\n", verb, mswords ) ;
+     }     
+
+     for ( i=0; i<mswords; i++ )
+     {
+          printf ("%s\n", misspelled[i] );
+     }
+}
