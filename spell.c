a8d053142028395a37194de580c8a70290360e99
Robert Smith <robert.p.smith@nyu.edu>
2019-09-12 21:26:50 -0400

    Added additional logic for detection of - and words ending in multiple punctuation marks

--- spell.c
+++ spell.c
@@ -60,7 +60,9 @@ int check_words(FILE* fp, hashmap_t hashtable[], char* misspelled[])
           i = 0 ;
           strcpy(word, "") ;
 
-          while ( ! isspace(c) )
+          if ( ispunct(c) ) { continue ; }
+
+          while (( ! isspace(c) ) && ( c != '-' ))
           {
                if ( feof(fp) ) { break ; }
                if ( i <= LENGTH)
@@ -81,10 +83,15 @@ int check_words(FILE* fp, hashmap_t hashtable[], char* misspelled[])
                word[0] = '\0' ;
           }
 
-          if ( i > 1 ) 
-          { 
-               if ( ispunct( word[i-1] ) ) { word[i-1] = '\0' ; }
+          while (( i > 1 ) && ( ispunct( word[i-1] )))
+          {
+               word[i-1] = '\0' ;
+               i-- ;
           }
+          //if ( i > 1 ) 
+          //{ 
+          //     if ( ispunct( word[i-1] ) ) { word[i-1] = '\0' ; }
+          //}
 
           if ( strlen(word) != 0 ) 
           { 
