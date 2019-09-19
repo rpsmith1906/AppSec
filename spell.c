42dad9a7f30919607c127a7f9faac6f63e1b437b
Robert Smith <robert.p.smith@nyu.edu>
2019-09-19 07:18:11 -0400

    Last update to fix memory issues from AFL

--- spell.c
+++ spell.c
@@ -6,14 +6,89 @@
 #include "dictionary.h"
 
 int hash_function(const char* word);
+bool isnumeric (const char* word);
 int check_words(FILE* fp, hashmap_t hashtable[], char* misspelled[]);
 bool load_dictionary(const char* dictionary_file, hashmap_t hashtable[]);
 bool check_word(const char* word, hashmap_t hashtable[]);
 
+bool isnumeric (const char* word)
+{
+     int i ;
+     char c='\0' ;
+     char number = 'u' ;
+
+     i=0 ;
+     while ( word[i] != '\0' )
+     {
+          c = word[i] ;
+          if ( i==0 )
+          {
+               if  (( c != '+' ) && (c != '-'))
+               {
+                    if ( isdigit (c) ) { number = 'n' ; }
+                    else { number = 'a' ; }
+               }
+          }
+          else
+          {
+               if ( isdigit (c) ) 
+               {
+                    if (  number == 'u' ) { number = 'n' ; }
+               }
+               else
+               {
+                    switch ( number )
+                    {
+                         case 'u' :
+                              number = 'a' ;
+                              break ;
+
+                         case 'n' :
+                              switch ( c )
+                              {
+                                   case '.' :
+                                        number = 'f' ;
+                                        break ;
+
+                                   case 'E' :
+                                        number = 'e' ;
+                                        break ;
+
+                                   default :
+                                        number = 'a' ;
+                              }
+                              break ;
+
+                         case 'f' :
+                              switch ( c )
+                              {
+                                   case 'E' :
+                                        number - 'e' ;
+                                        break ;
+
+                                   default :
+                                        number = 'a' ;
+                              }
+                     }
+               }
+          }
+
+          if ( number == 'a' ) { break ; }
+          else { i++ ; }
+
+     }
+   
+     i = strlen ( word ) ;
+     if ( ! isdigit( word[i-1] )) { number = 'a' ; }
+
+     if ( number == 'a' ) { return ( false ) ; }
+     else { return ( true ) ; }
+}
+
 bool check_word(const char* word, hashmap_t hashtable[])
 {
      int hash,i ;
-     node* node ;
+     node* node = NULL ;
      char lword[LENGTH + 1] ;
 
      for ( i=0; i<=LENGTH; i++ ) { lword[i] = '\0' ; }
@@ -31,7 +106,9 @@ bool check_word(const char* word, hashmap_t hashtable[])
 
      hash = hash_function(lword) ;
 
+     if (( hash > HASH_SIZE-1 ) || ( hash < 0 )) { return ( false ) ; }
      node = hashtable[hash] ;
+
      while ( node != NULL )
      {
           if ( strcmp(lword, node->word ) == 0 )
@@ -57,6 +134,7 @@ int check_words(FILE* fp, hashmap_t hashtable[], char* misspelled[])
      while ( ! feof( fp ))
      {
           c = fgetc(fp) ;
+          char *j ;
           i = 0 ;
           strcpy(word, "") ;
 
@@ -71,7 +149,6 @@ int check_words(FILE* fp, hashmap_t hashtable[], char* misspelled[])
                     i++ ;
                }
                c = fgetc(fp) ;
-
           }
 
           if ( i <= LENGTH ) { word[i] = '\0' ; }
@@ -79,6 +156,11 @@ int check_words(FILE* fp, hashmap_t hashtable[], char* misspelled[])
           { 
                word[LENGTH] = '\0' ;
                misspelled[mswords] = (char* )malloc(sizeof(char)*(LENGTH + 1)) ;
+               if ( misspelled[mswords] == NULL ) 
+               {
+                    printf("System exceeded available memory..\n") ;
+                    return (-1) ;
+               }
                strcpy ( misspelled[mswords++], word ) ;
                word[0] = '\0' ;
           }
@@ -88,17 +170,56 @@ int check_words(FILE* fp, hashmap_t hashtable[], char* misspelled[])
                word[i-1] = '\0' ;
                i-- ;
           }
-          //if ( i > 1 ) 
-          //{ 
-          //     if ( ispunct( word[i-1] ) ) { word[i-1] = '\0' ; }
-          //}
 
           if ( strlen(word) != 0 ) 
           { 
                if ( check_word(word, hashtable) == false ) 
                { 
-                    misspelled[mswords] = (char* )malloc(sizeof(char)*(LENGTH + 1)) ;
-                    strcpy ( misspelled[mswords++], word ) ;
+                    j = strchr(word, '-') ;
+
+                    if ( j != NULL ) 
+                    {
+                         j = strtok(word, "-" ) ;
+                         if ( check_word(word, hashtable) == false )
+                         {
+                              misspelled[mswords] = (char* )malloc(sizeof(char)*(LENGTH + 1)) ;
+                              if ( misspelled[mswords] == NULL ) 
+                              {
+                                   printf("System exceeded available memory..\n") ;
+                                   return (-1) ;
+                              }
+                              strcpy ( misspelled[mswords++], word ) ;
+
+                              j = strtok(NULL, "-") ;
+                              while ( j != NULL )
+                              {
+                                   if ( check_word(word, hashtable) == false )
+                                   {
+                                        if ( misspelled[mswords] == NULL ) 
+                                        {
+                                             printf("System exceeded available memory..\n") ;
+                                             return (-1) ;
+                                        }
+                                        misspelled[mswords] = (char* )malloc(sizeof(char)*(LENGTH + 1)) ;
+                                        strcpy ( misspelled[mswords++], word ) ;
+                                   }
+                              }
+
+                         }
+                    } 
+                    else
+                    {
+                         if ( ! isnumeric ( word ) )
+                         {
+                              misspelled[mswords] = (char* )malloc(sizeof(char)*(LENGTH + 1)) ;
+                              if ( misspelled[mswords] == NULL ) 
+                              {
+                                   printf("System exceeded available memory..\n") ;
+                                   return (-1) ;
+                              }
+                              strcpy ( misspelled[mswords++], word ) ;
+                         }
+                    }
                }
           }
      }
@@ -114,7 +235,7 @@ bool load_dictionary(const char* dictionary_file, hashmap_t hashtable[])
      node* head ;
      FILE *fptr;
 
-     for ( i=0 ; i<HASH_SIZE-1; i++ ) { hashtable[i] = NULL ; }
+     for ( i=0 ; i<HASH_SIZE; i++ ) { hashtable[i] = NULL ; }
 
      if (( fptr = fopen(dictionary_file, "r")) == NULL )
      {
--- spell_main.c
+++ spell_main.c
@@ -8,9 +8,11 @@
 int main (int argc, char *argv[] )
 {
      char *misspelled[1000] ;
-     char verb[5] = "were" ;
      FILE *fp ;
-     int i,mswords ;
+     int i ;
+     int mswords=0 ;
+     node* cnode ;
+     node* nnode ;
 
      hashmap_t hashtable[HASH_SIZE] ;
 
@@ -24,19 +26,38 @@ int main (int argc, char *argv[] )
      if (( fp = fopen(argv[1], "r")) == NULL )
      {
           printf ( "Error! could not open file \n" ) ;
+          return (1) ;
      }
 
-     load_dictionary( argv[2], hashtable ) ;
-     mswords=check_words(fp, hashtable, misspelled) ;
+     if ( load_dictionary( argv[2], hashtable ) == true )
+     {
+          mswords=check_words(fp, hashtable, misspelled) ;
+     }
+
+     fclose (fp) ;
+
+     for ( i=0; i<HASH_SIZE; i++ )
+     {
+          if ( hashtable[i] == NULL ) { continue ; }
+
+          cnode = hashtable[i] ;
+          while ( cnode != NULL )
+          {
+               nnode = cnode->next ;
+               free ( cnode ) ;
+               cnode = nnode ;
+          }
+     }
 
      if ( mswords > 0 ) 
      { 
-          if ( mswords == 1 ) { strcpy ( verb, "was" ) ; }
-          printf("There %s %i misspelled words found.\n\n", verb, mswords ) ;
-     }     
+          if ( mswords == 1 ) { printf("There was %i misspelled words found.\n\n", mswords ) ; }
+          else { printf("There were %i misspelled words found.\n\n", mswords ) ; }
 
-     for ( i=0; i<mswords; i++ )
-     {
-          printf ("%s\n", misspelled[i] );
+          for ( i=0; i<mswords; i++ )
+          {
+               printf ("%s\n", misspelled[i] );
+               free ( misspelled[i] ) ;
+          }
      }
 }
